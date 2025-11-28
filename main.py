0b6936c95ce779dd44f193ce56d3deff925db286
Carlo <81carlo@gmail.com>
2025-11-28 13:23:09 +0100

    funcion home en main.py para ver si un usuario esta logeado

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
-from fastapi import FastAPI, session,url_for
+from fastapi import FastAPI, request, redirect, session,url_for
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FastApiSessionCacheHandler
+
 
 app = FastAPI()
 app.config['SECRET_KEY'] = os.urandom(64)
@@ -17,11 +18,21 @@ sp_oauth = SpotifyOAuth(
     client_secret=client_secret,
     redirect_uri=redirect_uri,
     scope=scope,
-    cache_handler=cache_handler
+    cache_handler=cache_handler,
     show_dialog=True
 )
 
 sp = Spotify(auth_manager=sp_oauth)
+
+@app.get("/")
+async def home():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for(''))
+
+
+    
 
 if __name__ == '__main__':
     import uvicorn
