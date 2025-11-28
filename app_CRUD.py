d83bfca411bd094065ea63023ff276ffe97a36e4
Carlo <81carlo@gmail.com>
2025-11-28 17:42:54 +0100

    renombramos main.py como spotify_service.py

--- app_CRUD.py
+++ app_CRUD.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse
 from configuration.connection import DatabaseConnection
