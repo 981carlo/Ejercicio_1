b0deb98a526f4202aabccfbb175793c285362620
Carlo <81carlo@gmail.com>
2025-11-30 22:12:57 +0100

    ahora no hay ningun archivo ignorado

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto en fastAPI para crear una API que conecte una base de usuarios con Spotify
---
+++ spotify_sevice.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+# Carga de las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener credenciales de la api Spotify desde las variables de entorno
+
+client_id = os.getenv('client_id')
+client_secret = os.getenv('client_secret')
+
+url = 'https://accounts.spotify.com/api/token'
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+data = {'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret}
+
+oauth_response = requests.post(url, headers=headers, data=data)
+
+# Obtener el token de acceso del response
+
+access_token = oauth_response.json().get('access_token')
+
+# Función para buscar un álbum en Spotify usando la API
+
+def search_album(album_name: str):
+    
+    search_url = 'https://api.spotify.com/v1/search'
+    params = {'type': 'album', 'limit': 1, 'q': album_name}
+    headers = {'Authorization': f'Bearer {access_token}'}
+    
+    response = requests.get(search_url, headers=headers, params=params)
+    if response.status_code != 200:
+        return {"error": "Error al buscar el artista."}
+    album_name = response.json()['albums']['items'][0]['name']
+    artist_name = response.json()['albums']['items'][0]['artists'][0]['name']
+    
+    return album_name, artist_name
